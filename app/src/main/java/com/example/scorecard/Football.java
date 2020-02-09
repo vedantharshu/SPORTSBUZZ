d275926f871fed3e4ca10eafda5e3c6f97b2d472
shiv-srivastava88 <shiv.srivastava88@gmail.com>
2020-02-09 22:54:07 +0530

    i have given ui to football activity and created football_score activity

--- app/src/main/java/com/example/scorecard/Football.java
+++ app/src/main/java/com/example/scorecard/Football.java
@@ -2,13 +2,29 @@ package com.example.scorecard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Football extends AppCompatActivity {
+
+    Button fb_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_football);
+
+        fb_btn=(Button)findViewById(R.id.fb_btn);
+
+        fb_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent foot_score =new Intent(Football.this,football_score.class);
+                startActivity(foot_score);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/scorecard/football_score.java
@@ -0,0 +1,14 @@
+package com.example.scorecard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class football_score extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_football_score);
+    }
+}
